88eddc905992869876f84e83830e888aea51a60b
Zac Parker <Zac.Parker@nike.com>
2016-01-20 23:56:57 -0800

    move all logic in 'added' handler into if statement checking for artist name. this will prevent an interface from controlling a display it wasn't meant to display. remove changed listener, records will only be inserted or removed

--- client/helpers/display-barry-stone.js
+++ client/helpers/display-barry-stone.js
@@ -5,26 +5,23 @@ Template.displayBarryStone.helpers({
 
     query.observeChanges({
       added: function(id, fields) {
-        var selectedImageNumber = fields.selectedImageNumber;
-        var $selectedImgContainer = $('.user-selected-image-container');
-        var $selectedImg = $('.user-selected-image-container').find('img');
-        var fadeRate = 1500;
-
-        console.log(query.fetch());
-        $selectedImgContainer.html('<img src="/images/barry-stone/' + selectedImageNumber + '.jpg">');
-        $selectedImgContainer.fadeTo(fadeRate, 1);
-        setTimeout(function() {
-          $selectedImgContainer.fadeTo(fadeRate, 0, function() {
-            $selectedImg.remove();
-          })
-        }, 5000);
-      },
-      changed: function(id, fields) {
-        console.log('doc updated');
-        console.log(query.fetch());
+        if (fields.artist === "barry-stone") {
+          var selectedImageNumber = fields.selectedImageNumber;
+          var $selectedImgContainer = $('.user-selected-image-container');
+          var $selectedImg = $('.user-selected-image-container').find('img');
+          var fadeRate = 1500;
+          console.log(query.fetch());
+          $selectedImgContainer.html('<img src="/images/barry-stone/' + selectedImageNumber + '.jpg">');
+          $selectedImgContainer.fadeTo(fadeRate, 1);
+          setTimeout(function() {
+            $selectedImgContainer.fadeTo(fadeRate, 0, function() {
+              $selectedImg.remove();
+            })
+          }, 5000);
+        }
       },
       removed: function() {
-        console.log('previous action removed');
+          console.log('previous action removed');
       }
     });
 
