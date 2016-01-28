dbb5bffae0238412bc934997fdc97efe5b193cb4
Zac Parker <Zac.Parker@nike.com>
2016-01-27 20:13:45 -0800

    major restructuring and renaming to accommodate addition of multiple artists. added modal. added notes to display and interface code.

---
+++ client/helpers/stone/interface.js
@@ -0,0 +1,28 @@
+Template.interfaceStone.helpers({
+  templateGestures: {
+    'tap .trigger': function(event, templateInstance) {
+      event.preventDefault(); // when the user taps, don't follow the src link
+      var artist = "stone";
+      var actionType = event.type; // what type of event?
+      var selectedImageNumber = event.target.id; // looking for the id of the event target
+      // remove all events from database
+      Meteor.call('removeActions', function() { // clear all contents of the database and then
+        logActions(); // log to the console
+        console.log(actionType + ' performed via interface');
+        // save latest event to database
+        Actions.insert({
+          actionType: actionType,
+          artist: artist,
+          selectedImageNumber: selectedImageNumber,
+          createdAt: new Date() // current time
+        });
+      });
+    },
+    'tap .info-modal-open': function(event, templateInstance) {
+      AntiModals.overlay('modal-stone', { // when the user taps the 'T', open the modal
+        modal: true
+      });
+    }
+  }
+});
+
