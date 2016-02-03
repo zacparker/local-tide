c353995fb71133ad3c0594cef3744f99969e4601
Zac Parker <Zac.Parker@nike.com>
2016-02-03 15:20:15 -0800

    skyers - slowed animation speed, animated images now pause in middle. added mechanism to block taps that occur while an animation is is progress

--- client/helpers/skyers/display.js
+++ client/helpers/skyers/display.js
@@ -1,23 +1,33 @@
 Template.displaySkyers.helpers({
 
   actionPerformed: function() {
+    var blocking;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
 
     query.observeChanges({ // listen to changes to the collection
 
       added: function(id, fields) { // if anything is added to the collection
-        if (fields.artist === "skyers") { // if the added item matches this artist
-          skyersCount = typeof skyersCount === "undefined" ? 1 : skyersCount; // Defined in the global namespace to keep track of the count across actions
-          $('#' + skyersCount).addClass('active').removeClass('hidden').animate({top: "100%"}, 5000, function() {
-            $('#' + skyersCount).removeClass('active').addClass('hidden');
-
-            // Increment the mattesCounter
+        if (fields.artist === "skyers" && blocking !== true) { // if the added item matches this artist
+          blocking = true;
+          skyersCount = typeof skyersCount === "undefined" ? 0 : skyersCount; // Defined in the global namespace to keep track of the count across actions
+          if (skyersCount >= 1) {
+            $('#' + skyersCount).addClass('active').removeClass('hidden').animate({top: 0}, 5000);
+            setTimeout(function() {
+              $('#' + skyersCount).animate({top: "100%"}, 5000, function() {
+                $('#' + skyersCount).removeClass('active').addClass('hidden');
+                // Increment the mattesCounter
+                skyersCount++;
+                // Reset the mattesCounter
+                if (skyersCount >= 17) {
+                  skyersCount = 1;
+                }
+                blocking = false;
+              });
+            }, 500);
+          } else {
             skyersCount++;
-            // Reset the mattesCounter
-            if (skyersCount >= 17) {
-              skyersCount = 1;
-            }
-          });
+            blocking = false;
+          }
         }
       },
 
