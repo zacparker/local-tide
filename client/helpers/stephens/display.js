8921c2557518d6c7fa55aa4929791e1229edd87a
Zac Parker <Zac.Parker@nike.com>
2016-02-04 18:59:45 -0800

    add interface files, some updates to barnett files, add stephens exhibit

--- client/helpers/stephens/display.js
+++ client/helpers/stephens/display.js
@@ -1,23 +1,16 @@
 Template.displayStephens.helpers({
   actionPerformed: function() {
-    var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
-    query.observeChanges({ // listen to changes to the collection
-      added: function(id, fields) { // if anything is added to the collection
-        if (fields.artist === "stephens") { // if the added item matches this artist
-          var selectedImageNumber = fields.selectedImageNumber;
-          var $selectedImgContainer = $('.user-selected-image-container');
-          var $selectedImg = $('.user-selected-image-container').find('img'); // the image that the user selected via the interface
-          var fadeRate = 1500; // 1.5 seconds
-          $selectedImgContainer.html('<img src="/media/stephens/display/' + selectedImageNumber + '.jpg">'); // add the selected image to it's container
-          $selectedImgContainer.fadeTo(fadeRate, 1); // fade the image in
-          setTimeout(function() { // after 5 seconds, fade the image out and then remove it from the DOM
-            $selectedImgContainer.fadeTo(fadeRate, 0, function() {
-              $selectedImg.remove();
-            })
-          }, 5000);
+      videoIsPlaying = true;
+      var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
+      query.observeChanges({ // listen to changes to the collection
+        added: function(id, fields) { // if anything is added to the collection
+          if (fields.artist === "stephens") { // if the added item matches this artist
+            var v = $('#display-stephens-video').get(0);
+            if (videoIsPlaying) { v.pause() } else {  v.play() }
+            videoIsPlaying = !videoIsPlaying ? true : false;
+          }
         }
-      }
-    });
-    return query;
-  }
+      });
+      return query;
+    }
 });
