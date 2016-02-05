04f0b0b5698acc389b0cb5b7c38e028b742abcd8
Zac Parker <Zac.Parker@nike.com>
2016-02-05 00:02:48 -0800

    add maggio exhibit

--- client/helpers/maggio/display.js
+++ client/helpers/maggio/display.js
@@ -1,20 +1,42 @@
 Template.displayMaggio.helpers({
   actionPerformed: function() {
+    maggioDisplayCount = 0;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
     query.observeChanges({ // listen to changes to the collection
       added: function(id, fields) { // if anything is added to the collection
-        if (fields.artist === "magdy") { // if the added item matches this artist
-          var selectedImageNumber = fields.selectedImageNumber;
-          var $selectedImgContainer = $('.user-selected-image-container');
-          var $selectedImg = $('.user-selected-image-container').find('img'); // the image that the user selected via the interface
-          var fadeRate = 1500; // 1.5 seconds
-          $selectedImgContainer.html('<img src="/media/magdy/display/' + selectedImageNumber + '.jpg">'); // add the selected image to it's container
-          $selectedImgContainer.fadeTo(fadeRate, 1); // fade the image in
-          setTimeout(function() { // after 5 seconds, fade the image out and then remove it from the DOM
-            $selectedImgContainer.fadeTo(fadeRate, 0, function() {
-              $selectedImg.remove();
-            })
-          }, 5000);
+        if (fields.artist === "maggio") { // if the added item matches this artist
+          var $topNav = $('.top-nav');
+          var $sidebar = $('.sidebar');
+          var displayTopNav = function() {
+            $topNav.animate({top: 0}, 2000);
+            incrementCount();
+          },
+          displaySidebar = function() {
+            $sidebar.animate({top: "39px"}, 4000);
+            incrementCount();
+          },
+          reset = function() {
+            $topNav.animate({top: "-40px"}, 2000);
+            $sidebar.animate({top: "-730px"}, 4000);
+            maggioDisplayCount = 1;
+          },
+          incrementCount = function() {
+            maggioDisplayCount++;
+          };
+          switch(maggioDisplayCount) {
+            case 0:
+              incrementCount();
+              break;
+            case 1:
+              displayTopNav();
+              break;
+            case 2:
+              displaySidebar();
+              break;
+            case 3:
+              reset();
+              break;
+          }
         }
       }
     });
