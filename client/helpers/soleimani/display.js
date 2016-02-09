41b3a33e55fe30ed292a72edef3bde9a7f82ca61
Zac Parker <Zac.Parker@nike.com>
2016-02-08 19:29:26 -0800

    fix soleimani exhibit

--- client/helpers/soleimani/display.js
+++ client/helpers/soleimani/display.js
@@ -1,17 +1,24 @@
 Template.displaySoleimani.helpers({
   actionPerformed: function() {
+    soleimaniCount = 0;
     videoIsPlaying = true;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
     query.observeChanges({ // listen to changes to the collection
       added: function(id, fields) { // if anything is added to the collection
-        if (fields.artist === "soleimani") { // if the added item matches this artist
+        if (fields.artist === "soleimani" && soleimaniCount >= 1) { // if the added item matches this artist
+          $('img').remove();
           var v = $('#display-soleimani-video').get(0);
+          if ($(v).hasClass('hidden')) {
+            $(v).removeClass('hidden')
+          }
           if (videoIsPlaying) {
             v.pause();
           } else {
             v.play();
           }
           videoIsPlaying = !videoIsPlaying ? true : false;
+        } else if (soleimaniCount <= 0) {
+          soleimaniCount++;
         }
       }
     });
