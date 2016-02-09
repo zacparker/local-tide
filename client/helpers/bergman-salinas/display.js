d9b22ba18be42938093b4b79b75e77d7c59a48f0
Zac Parker <Zac.Parker@nike.com>
2016-02-08 21:47:11 -0800

    fix bergman-salinas exhibit

--- client/helpers/bergman-salinas/display.js
+++ client/helpers/bergman-salinas/display.js
@@ -1,12 +1,21 @@
 Template.displayBergmanSalinas.helpers({
   actionPerformed: function() {
-    initializing = true;
+    var initializing = true;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
     query.observeChanges({ // listen to changes to the collection
       added: function(id, fields) { // if anything is added to the collection
         if (fields.artist === "bergman-salinas" && !initializing) { // if the added item matches this artist
+          var togglePlay = function(video) {
+            if (video.paused) {
+              video.play();
+            } else {
+              video.pause();
+            }
+          }
           $('#display-bergman-salinas-video').toggleClass('hidden');
           $('#display-bergman-salinas-video-selected').toggleClass('hidden');
+          togglePlay($('#display-bergman-salinas-video').get(0));
+          togglePlay($('#display-bergman-salinas-video-selected').get(0));
         }
       }
     });
