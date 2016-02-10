8b439ce70411298b2649a1e251824a8ea0a069b5
Zac Parker <Zac.Parker@nike.com>
2016-02-09 23:33:50 -0800

    fix skyers logic bug

--- client/helpers/skyers/display.js
+++ client/helpers/skyers/display.js
@@ -1,8 +1,8 @@
 Template.displaySkyers.helpers({
   actionPerformed: function() {
     skyersCount = 0;
-    skyersDisplayCount = 0; // Defined in the global namespace to keep track of the count across actions
-    var isAnimationInProgress;
+    skyersDisplayCount = 1; // Defined in the global namespace to keep track of the count across actions
+    var isAnimationInProgress = false;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
     query.observeChanges({ // listen to changes to the collection
       added: function(id, fields) { // if anything is added to the collection
@@ -10,24 +10,19 @@ Template.displaySkyers.helpers({
           location.href = "/" + fields.artist + "/display";
         } else if (!fields.sender && fields.artist === "skyers" && isAnimationInProgress !== true && skyersCount) { // if the added item matches this artist
           isAnimationInProgress = true;
-          if (skyersDisplayCount >= 1) {
-            $('#' + skyersDisplayCount).addClass('active').removeClass('hidden').animate({top: 0}, 5000);
-            setTimeout(function() {
-              $('#' + skyersDisplayCount).animate({top: "100%"}, 5000, function() {
-                $('#' + skyersDisplayCount).removeClass('active').addClass('hidden');
-                // Increment the skyersDisplayCounter
-                skyersDisplayCount++;
-                // Reset the skyersDisplayCounter
-                if (skyersDisplayCount >= 17) {
-                  skyersDisplayCount = 1;
-                }
-                isAnimationInProgress = false;
-              });
-            }, 500);
-          } else {
-            skyersDisplayCount++;
-            isAnimationInProgress = false;
-          }
+          $('#' + skyersDisplayCount).addClass('active').removeClass('hidden').animate({top: 0}, 5000);
+          setTimeout(function() {
+            $('#' + skyersDisplayCount).animate({top: "100%"}, 5000, function() {
+              $('#' + skyersDisplayCount).removeClass('active').addClass('hidden');
+              // Increment the skyersDisplayCounter
+              skyersDisplayCount++;
+              // Reset the skyersDisplayCounter
+              if (skyersDisplayCount >= 17) {
+                skyersDisplayCount = 1;
+              }
+              isAnimationInProgress = false;
+            });
+          }, 500);
         } else if (fields.artist === "skyers" && skyersCount === 0) {
           skyersCount++;
         }
