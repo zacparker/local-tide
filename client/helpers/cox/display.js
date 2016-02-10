16b736a212d511d7d4970f48cc4339f1fd598554
Zac Parker <Zac.Parker@nike.com>
2016-02-10 13:16:21 -0800

    add cox exhibit

--- client/helpers/cox/display.js
+++ client/helpers/cox/display.js
@@ -7,17 +7,14 @@ Template.displayCox.helpers({
         if (fields.sender === "1" && coxCount) {
           location.href = "/" + fields.artist + "/display";
         } else if (!fields.sender && fields.artist === "cox" && coxCount) { // if the added item matches this artist
-          var selectedImageNumber = fields.selectedImageNumber;
-          var $selectedImgContainer = $('.user-selected-image-container');
-          var $selectedImg = $('.user-selected-image-container').find('img'); // the image that the user selected via the interface
-          var fadeRate = 1500; // 1.5 seconds
-          $selectedImgContainer.html('<img src="/media/cox/display/' + selectedImageNumber + '.jpg">'); // add the selected image to it's container
-          $selectedImgContainer.fadeTo(fadeRate, 1); // fade the image in
-          setTimeout(function() { // after 5 seconds, fade the image out and then remove it from the DOM
-            $selectedImgContainer.fadeTo(fadeRate, 0, function() {
-              $selectedImg.remove();
-            })
-          }, 5000);
+          var $hiddenImages = $('img.hidden');
+          var thisImage = $hiddenImages.get(0);
+          var $thisImage = $(thisImage);
+          $thisImage.removeClass('hidden');
+          if (!$thisImage.length) {
+            $('img').addClass('hidden');
+            $('img.bg').removeClass('hidden');
+          }
         } else if (fields.artist === "cox" && coxCount === 0) {
           coxCount++;
         }
