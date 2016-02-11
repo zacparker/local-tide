6695d22f2acab541bbbda404e3a18ddd4b084289
Zac Parker <Zac.Parker@nike.com>
2016-02-10 21:22:20 -0800

    fix stephens exhibit

--- client/helpers/stephens/display.js
+++ client/helpers/stephens/display.js
@@ -1,16 +1,13 @@
 Template.displayStephens.helpers({
   actionPerformed: function() {
     stephensCount = 0;
-    videoIsPlaying = true;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
     query.observeChanges({ // listen to changes to the collection
       added: function(id, fields) { // if anything is added to the collection
         if (fields.sender === "5" && stephensCount) {
           location.href = "/" + fields.artist + "/display";
         } else if (!fields.sender && fields.artist === "stephens" && stephensCount) { // if the added item matches this artist
-          var v = $('#display-stephens-video').get(0);
-          if (videoIsPlaying) { v.pause() } else {  v.play() }
-          videoIsPlaying = !videoIsPlaying ? true : false;
+          $('.stephens-video').toggleClass('hidden');
         } else if (fields.artist === "stephens" && stephensCount === 0) {
           stephensCount++;
         }
