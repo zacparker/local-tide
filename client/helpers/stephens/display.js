f1e51ecda9ecf835674a5924d5fadaafb5a0d9bc
Zac Parker <Zac.Parker@nike.com>
2016-02-10 20:45:06 -0800

    remove collection hooks package, add still images in place of videos

--- client/helpers/stephens/display.js
+++ client/helpers/stephens/display.js
@@ -9,7 +9,13 @@ Template.displayStephens.helpers({
           location.href = "/" + fields.artist + "/display";
         } else if (!fields.sender && fields.artist === "stephens" && stephensCount) { // if the added item matches this artist
           var v = $('#display-stephens-video').get(0);
-          if (videoIsPlaying) { v.pause() } else {  v.play() }
+          if (videoIsPlaying) {
+            $(v).addClass('hidden');
+            v.pause();
+          } else {
+            $(v).removeClass('hidden');
+            v.play();
+          }
           videoIsPlaying = !videoIsPlaying ? true : false;
         } else if (fields.artist === "stephens" && stephensCount === 0) {
           stephensCount++;
