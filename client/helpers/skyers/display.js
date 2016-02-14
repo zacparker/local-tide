57ba50394e263a21bf55389f4f42cbc73cadc9af
Zac Parker <Zac.Parker@nike.com>
2016-02-14 10:43:02 -0800

    re-order skyers images, fix bug i found during opening

--- client/helpers/skyers/display.js
+++ client/helpers/skyers/display.js
@@ -1,7 +1,7 @@
 Template.displaySkyers.helpers({
   actionPerformed: function() {
     skyersCount = 0;
-    skyersDisplayCount = 1; // Defined in the global namespace to keep track of the count across actions
+    skyersDisplayCount = 0; // Defined in the global namespace to keep track of the count across actions
     var isAnimationInProgress = false;
     var query = Actions.find(); // find all data in Actions collection, assign it to 'query'
     query.observeChanges({ // listen to changes to the collection
@@ -10,15 +10,19 @@ Template.displaySkyers.helpers({
           location.href = "/" + fields.artist + "/display";
         } else if (!fields.sender && fields.artist === "skyers" && isAnimationInProgress !== true && skyersCount) { // if the added item matches this artist
           isAnimationInProgress = true;
-          $('#' + skyersDisplayCount).addClass('active').removeClass('hidden').animate({top: 0}, 5000);
+          var $images = $('img');
+          var thisImage = $images.get(skyersDisplayCount);
+          var $thisImage = $(thisImage);
+          $thisImage.addClass('active').removeClass('hidden').animate({top: 0}, 5000);
           setTimeout(function() {
-            $('#' + skyersDisplayCount).animate({top: "100%"}, 5000, function() {
-              $('#' + skyersDisplayCount).removeClass('active').addClass('hidden');
+            $thisImage.animate({top: "100%"}, 5000, function() {
+              $thisImage.removeClass('active').addClass('hidden');
+              $thisImage.css({top: "-100%"});
               // Increment the skyersDisplayCounter
               skyersDisplayCount++;
               // Reset the skyersDisplayCounter
-              if (skyersDisplayCount >= 17) {
-                skyersDisplayCount = 1;
+              if (skyersDisplayCount >= 2) {
+                skyersDisplayCount = 0;
               }
               isAnimationInProgress = false;
             });
